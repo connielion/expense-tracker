ce43030cb4dfbc2aebe975352c2b50427454d093
Connie <clai011@ucr.edu>
2018-07-26 03:21:11 -0700

    favicon added into assets and linked

--- src/app/tracker/tracker.component.ts
+++ src/app/tracker/tracker.component.ts
@@ -2,19 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { ITEMS } from '../mock-items';
 import { Item } from '../items';
 
-
 @Component({
   selector: 'app-tracker',
   templateUrl: './tracker.component.html',
   styleUrls: ['./tracker.component.css']
 })
+
 export class TrackerComponent implements OnInit {
 
   newItem: Item = new Item();
 
   items = ITEMS;
 
+  total: number = 0;
+
   constructor() { }
+
 // delete item function
   deleteItem(del) {
     const rm: number = this.items.indexOf(del);
@@ -24,17 +27,27 @@ export class TrackerComponent implements OnInit {
 
   }
 
-  // add item
+  // add item function
 
   addItem() {
     const prev = this.items.length - 1;
     const p = this.items[prev];
     this.items.push(this.newItem);
     this.newItem = new Item();
-
   }
 
+  findTotal() {
+    let total = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      let item = this.items[i];
+
+    }
+    return total;
+  }
+
+
   ngOnInit() {
+
   }
 
 }
